f29ecdbebdfbacdcf01641d7c7e63ffda780f207
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-03 12:04:31 +0530

    Enemy Script and Setup

---
+++ StreetFighter/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private int facingDirection = 1;
+
+    [Header("AttackSettings")]
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float attackRange;
+    [SerializeField] private LayerMask playerLayer;
+
+    [Header("References")]
+    [SerializeField] private Transform player;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private bool isChasing;
+    [SerializeField] private Animator animator;
+
+    [Header("Attack Settings")]
+    [SerializeField] private float attackCoolDown = 1f;
+    [SerializeField] bool isAttacking;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if(player == null)
+        {
+            return;
+        }
+
+        float distance = Vector2.Distance(player.position, transform.position);
+
+        if(distance <= attackRange)
+        {
+            isChasing = false;
+            rb.linearVelocity = Vector2.zero;
+            animator.SetBool("isRunning", false);
+
+            if(!isAttacking)
+            {
+                StartCoroutine(AttackCoroutine());
+            }
+
+            return;
+        }
+
+        if(isChasing == true)
+        {
+            if(player.position.x > transform.position.x && facingDirection == 1 || player.position.x <  transform.position.x && facingDirection == -1)
+            {
+                Flip();
+            }
+        }
+
+        if(isChasing == true)
+        {
+            Chase();
+        }
+    }
+
+    void Chase()
+    {
+        animator.SetBool("isRunning", true);
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    IEnumerator AttackCoroutine()
+    {
+        isAttacking = true;
+
+        while(true)
+        {
+            if(player == null)
+            {
+                break;
+            }
+
+            float distance = Vector2.Distance(transform.position, player.position);
+            if(distance >= attackRange)
+            {
+                break;
+            }
+
+            Attack();
+
+            yield return new WaitForSeconds(attackCoolDown);
+        }
+
+        isAttacking = false;
+    }
+
+    void Attack()
+    {
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPoint.position,attackRange,playerLayer);
+        foreach(Collider2D hit in hitPlayer)
+        {
+            if(player == null)
+            {
+                continue;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            animator.SetBool("isRunning", false);
+            isChasing = false;
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+}
